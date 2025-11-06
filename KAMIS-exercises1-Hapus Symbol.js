8cc7b5b14c5dcd826120c423a9c6c0901d8f181e
Aryanda Ramadhan <aryandakura@gmail.com>
2025-11-06 12:55:14 +0700

    REGEX coding

---
+++ KAMIS-exercises1-Hapus Symbol.js
@@ -0,0 +1,44 @@
+// Cara Penulisan REGEX
+var regexContohSatu = new RegExp('Arya'); //penulisan Regex yang dinamis, bisa diubah variabelnya.
+var regexContohDua = /abc/; // bisa tapi variabel tetap tidak bisa diubah.
+
+//TEST
+var message = 'Regex itu Mudah!';
+console.log(/[A-Z]/.test(message));
+
+var messageAllLowerCase = 'regex itu susah!';
+console.log(/[A-Z]/.test(messageAllLowerCase));
+
+//Menggunakan RegExp object
+var regexPattern = new RegExp('[A-Z]');
+var message = 'Regex itu Mudah!';
+var messageAllLowerCase = 'regex itu sulit?';
+console.log(regexPattern.test(message));
+console.log(regexPattern.test(messageAllLowerCase));
+
+//SPLIT
+var str = 'belajar regex itu menyenangkan';
+console.log(str.split(/\s/));
+
+//REPLACE
+var stringAwal = 'Belajar Regex itu sulit!';
+stringHasil = stringAwal.replace(/sulit/, 'mudah');
+console.log(stringHasil);
+
+// MATCH
+var message = 'Regex Itu Mudah DEEEEh!';
+console.log(message.match(/e/));
+console.log(message.match(/e/g));
+console.log(message.match(/e/gi));
+
+var string = 'Walaupun regex banyak mengandung simbol, tapi tidak serumit seperti !@#%^%#$*( , ^%&*!!^&'
+console.log(string.match(/[a-z]+/gi));
+
+var string = 'Walaupun regex banyak mengandung simbol, tapi tidak serumit seperti'
+console.log(string.match(/[a-z]/gi));
+
+function hapusSimbol(str){
+    var string = str.match(/[a-z\d]+/gi);
+    return string.join('');
+}
+console.log(hapusSimbol('test%$4aa'));
