92ef6f7d6f8d856847531b55a8161344e61d6996
Aryanda Ramadhan <aryandakura@gmail.com>
2025-11-06 09:12:23 +0700

    tambah kode

--- RABU-exercises2-Melee Ranged Grouping.js
+++ RABU-exercises2-Melee Ranged Grouping.js
@@ -1,17 +1,22 @@
-function meleRangedGrouping(str){
-    if(str.length === 0)
-        return [];
-        let heroes = str.split(',');
-        let ranged = [];
-        let melee = [];
-    for(let i = 0; i < heroes.length; i++){
-        let [name, type] = heroes[i].split(',');
-        if(type === 'ranged'){
-            ranged.push(name);
-        } else if(type === 'melee') {
-            melee.push(name);
-        }
 
-        }
-        return [ranged, melee];
+
+
+    function meleeRangedGrouping(str){ //membuat fungsi dengan nama meleeRangedGrouping dengan satu parameter STR.
+        if(str.length === 0) // jika tidak ada input / 0
+            return []; // maka kembalikan nilai kosong.
+
+            let heroes = str.split(','); // memecah str bedasarkan koma
+            let ranged = []; // buat wadah kosong untuk type heroes ranged
+            let melee = []; // buat wadah kosong untuk type heroes melee
+
+            for( let i = 0; i < heroes.length; i++){ // loop, untuk perikasa setiap heroes dalam array.
+                let [name, type] = heroes[i].split('-'); //memisahkan data hero menjadi nama dan tipe
+                if(type === 'Ranged'){ // apabila type sama dengan Ranged
+                    ranged.push(name); // maka akan masuk ke dalam grup Ranged.
+                } else if (type === 'Melee'){ // apabila type sama dengan Melee
+                    melee.push(name); // maka akan masuk dalam grup melee
+                }
+            }
+        return [ranged, melee]; // kembalikan nilai ranged & melee 
     }
+    console.log(meleeRangedGrouping('Razor-Ranged,Invoker-Ranged,Meepo-Melee,Axe-Melee,Sniper-Ranged'));
